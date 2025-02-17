178bca5acc75a7a74f45be69411f40e303f63039
Lukas Meysel <lukas.meysel@easycore.com>
2025-02-17 22:14:52 +0100

    3-Update Formatting for Plugbox, add PlugEnum

--- src/plugbox.c
+++ src/plugbox.c
@@ -1,28 +1,22 @@
 #include "plugbox.h"
 
-gate_ctx_t* gates[GATES_MAX_NUM];
+gate_ctx_t *gates[GATES_MAX_NUM];
 
-void plugbox_init(void)
-{
-	for (int i = 0; i < GATES_MAX_NUM; i++)
-{
-		gates[i] = &panic_gate;
-	}
+void plugbox_init(void) {
+  for (int i = 0; i < GATES_MAX_NUM; i++) {
+    gates[i] = &panic_gate;
+  }
 }
 
-void plugbox_assign(uint32_t interrupt_num, gate_ctx_t *gate)
-{
-	if(interrupt_num < GATES_MAX_NUM)
-	{
-		gates[interrupt_num] = gate;
-	}
+void plugbox_assign(plugbox_vector_t vec_num, gate_ctx_t *gate) {
+  if (vec_num < GATES_MAX_NUM) {
+    gates[vec_num] = gate;
+  }
 }
 
-gate_ctx_t *plugbox_report(uint32_t interrupt_num)
-{
-	if (interrupt_num >= GATES_MAX_NUM)
-	{
-		interrupt_num = GATES_MAX_NUM - 1; // this will cause a panic
-	}
-		return gates[interrupt_num];
+gate_ctx_t *plugbox_report(plugbox_vector_t vec_num) {
+  if (vec_num >= GATES_MAX_NUM) {
+    vec_num = GATES_MAX_NUM - 1; // this will cause a panic
+  }
+  return gates[vec_num];
 }
--- src/plugbox.h
+++ src/plugbox.h
@@ -7,8 +7,14 @@
 
 #define GATES_MAX_NUM 32
 
-extern gate_ctx_t* gates[GATES_MAX_NUM];
+extern gate_ctx_t *gates[GATES_MAX_NUM];
 
-void plugbox_init(void);
-void plugbox_assign(uint32_t interrupt_num, gate_ctx_t *gate);
-gate_ctx_t *plugbox_report(uint32_t interrupt_num);
+typedef enum {
+  PLUG_SYSTEM_TIMER = 1,
+  PLUG_BUTTON = 2,
+  PLUG_PANIC = 31,
+} plugbox_vector_t;
+
+extern void plugbox_init(void);
+extern void plugbox_assign(plugbox_vector_t vec_num, gate_ctx_t *gate);
+extern gate_ctx_t *plugbox_report(plugbox_vector_t vec_num);
